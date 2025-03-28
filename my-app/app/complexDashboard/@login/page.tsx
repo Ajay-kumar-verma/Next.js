1a3183507fdcc01d6632c4571969baedb18d8ad0
Ajay <ajaykver9@gmail.com>
2025-03-28 13:59:17 +0530

    conditional rendering

---
+++ my-app/app/complexDashboard/@login/page.tsx
@@ -0,0 +1,3 @@
+export default function Login(){
+  return <h1>please login to continue</h1>
+}
--- my-app/app/complexDashboard/layout.tsx
+++ my-app/app/complexDashboard/layout.tsx
@@ -2,17 +2,23 @@ export default function RootLayout({
   children,
   user,
   notification,
+  login,
 }: Readonly<{
   children: React.ReactNode;
   user: React.ReactNode;
   notification: React.ReactNode;
+  login: React.ReactNode;
 }>) {
-  return (
+  const isLoggedIn = true;
+
+  return isLoggedIn ? (
     <body>
       {children}
       {user}
       {notification}
     </body>
+  ) : (
+    login
   );
 }
 
